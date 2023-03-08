7e9377ee09d398d364a794d2ab6094cf1cbb467a
Paul J <jorpaulvincent@gmail.com>
2023-03-08 21:04:23 +0800

    install axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    vuetify,
+    render: (h) => h(App),
+}).$mount("#app");
